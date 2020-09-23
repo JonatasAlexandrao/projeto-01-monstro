602e9dc10b312ee296cd2f3cbaeb21b6c0be6fb5
Jônatas <jonatas-flash@hotmail.com>
2020-09-23 16:05:07 -0300

    feito layout dos botoes

--- app.js
+++ app.js
@@ -3,14 +3,21 @@ new Vue({
     data: {
         playerLife: 0,
         monsterLife: 100,
+        running: false,
     },
     computed: {
         hasResult() {
             return this.playerLife == 0 || this.monsterLife == 0
-        }
+        },
 
     },
     methods: {
+        startGame() {
+
+            this.running = true
+            this.playerLife = 100
+            this.monsterLife = 100
+        }
 
     },
     watch: {
