aa89b1719542f368b5ab7cbadb95303f6cbafa3c
Jônatas <jonatas-flash@hotmail.com>
2020-09-23 17:46:03 -0300

    implementado a lista de log das ações

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 new Vue({
     el: '#app',
     data: {
-        playerLife: 0,
+        playerLife: 100,
         monsterLife: 100,
         running: false,
+        logs: [],
+        textAtackPlayer: 'Jogador Atingiu o Monstro com ',
+        textHealPlayer: 'Jogador se Curou em ',
+        textAtackMonster: 'Monstro Atingiu o Jogador com ',
     },
     computed: {
         hasResult() {
@@ -17,40 +21,57 @@ new Vue({
             this.running = true
             this.playerLife = 100
             this.monsterLife = 100
+            this.logs = []
         },
         attack(especial) {
+            
             this.attackPlayer(especial)
-            this.attackMonster()
-
+            this.attackMonster()  
             
         },
         attackPlayer(especial) {
+            let hurt
             if(especial){
-                this.monsterLife -= this.getRandom(10, 15)
+                hurt = this.getRandom(10, 15)
+                this.monsterLife -= hurt
             }
             else{
-                this.monsterLife -= this.getRandom(5, 10)
+                hurt = this.getRandom(5, 10)
+                this.monsterLife -= hurt
             }
 
             if(this.monsterLife < 0)
                 this.monsterLife = 0
+
+            this.registerLog(this.textAtackPlayer + hurt, 'player')
         },
         attackMonster() {
-            this.playerLife -= this.getRandom(5, 13)
+            
+            const hurt = this.getRandom(5, 13)
+            this.playerLife -= hurt
+
             if(this.playerLife < 0)
                 this.playerLife = 0
+
+            this.registerLog(this.textAtackMonster + hurt, 'monster')
         },
         getRandom(min, max) {
             const value = Math.random() * (max - min) + min
             return Math.round(value)
         },
         heal() {
-            this.playerLife += this.getRandom(8, 15)
+            let heal = this.getRandom(8, 15)
+            this.playerLife += heal
             //const heal = this.getRandom(5, 15)
             //this.playerLife = Math.min(this.playerLife + heal, 100)
             this.attackMonster()
             if(this.playerLife > 100)
                 this.playerLife = 100
+
+            this.registerLog(this.textHealPlayer + heal, 'player')
+        },
+        registerLog(text, cls) {
+            this.logs.unshift({ text, cls }) //adiciona sempre na primeira posição do array
         }
 
     },
